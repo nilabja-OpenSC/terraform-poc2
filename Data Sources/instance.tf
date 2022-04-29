570e48c92bcbc34809776e7d1bd4d9be1a7ab0c3
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-04-29 17:40:53 +0530

    Add files via upload

--- Data Sources/instance.tf
+++ Data Sources/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "MyFirstInstnace" {
   ami           = data.aws_ami.latest-amazon-linux.id
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.avilable.names[1]
-  security_groups = aws_security_group.webservers_sg.id
+  security_groups = [aws_security_group.webservers_sg.id]
   provisioner "local-exec" {
     command = "echo ${aws_instance.MyFirstInstnace.private_ip} >> my_private_ips.txt"
   }
