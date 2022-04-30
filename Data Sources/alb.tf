4e7a7b9548171574ccfed5b197f0d038881d4cb8
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-04-30 10:59:58 +0530

    Add files via upload

--- Data Sources/alb.tf
+++ Data Sources/alb.tf
@@ -11,9 +11,21 @@ resource "aws_lb" "project-load-balancer" {
 # Add Target Group
 resource "aws_lb_target_group" "load-balancer-target-group" {
   name     = "load-balancer-target-group"
+  target_type = "alb"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.test_vpc.id
+
+  health_check {
+    path = "/"
+    port = 80
+    protocol = "HTTP"
+    healthy_threshold = 6
+    unhealthy_threshold = 2
+    timeout = 2
+    interval = 5
+    matcher = "200"  # has to be HTTP 200 or fails
+  }
 }
 
 # Adding HTTP listener
