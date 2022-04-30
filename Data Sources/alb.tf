4d075e890521d3d589292fc07ebde18c328bb50b
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-04-30 12:14:52 +0530

    Add files via upload

--- Data Sources/alb.tf
+++ Data Sources/alb.tf
@@ -24,7 +24,7 @@ resource "aws_lb_target_group" "load-balancer-target-group" {
     unhealthy_threshold = 2
     timeout = 2
     interval = 30
-    matcher = "200"  # has to be HTTP 200 or fails
+    #matcher = "200"  # has to be HTTP 200 or fails
   }
 }
 
