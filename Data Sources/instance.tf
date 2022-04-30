0a4f10ab41011dc5465d6e8a7310f6d7dd873ca9
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-04-30 14:54:29 +0530

    Add files via upload

--- Data Sources/instance.tf
+++ Data Sources/instance.tf
@@ -53,7 +53,7 @@ resource "aws_autoscaling_group" "project_webserver" {
   name                      = "project_WebServers"
   max_size                  = 3
   min_size                  = 1
-  health_check_grace_period = 30
+  health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 2
   force_delete              = true
